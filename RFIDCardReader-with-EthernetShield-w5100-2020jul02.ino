e71b6c2aea02c54facfeb26ec88ef451831bb460
Ali kHalegh Verdi <14966956+alikhaleghverdi@users.noreply.github.com>
2020-07-10 23:30:18 +0430

    Add files via upload

--- RFIDCardReader-with-EthernetShield-w5100-2020jul02.ino
+++ RFIDCardReader-with-EthernetShield-w5100-2020jul02.ino
@@ -54,43 +54,44 @@ void loop() {
   clientRequest();
   handleRequest();
 
-  if(!mfrc522.PICC_IsNewCardPresent())
-  {
-    return;
-  }
-  if(!mfrc522.PICC_ReadCardSerial())
-  {
-    return;
-  }
-  Serial.print("UID Tag : ");
-  String content= "";
-  byte letter;
-  for(byte i = 0;i<mfrc522.uid.size; i++)
-  {
-    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-    Serial.print(mfrc522.uid.uidByte[i], HEX);
-    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-    content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  content.toUpperCase();
-  if(content.substring(1) == "09 C2 E6 A2")
-  {
-    Serial.println("WELCOME ALI");
-    Serial.println();
-    delay(3000);
-  }
-  else if(content.substring(1) == "59 91 1C 98")
-  {
-    Serial.println("WELCOME REZA");
-    Serial.println();
-    delay(3000);
-  }
-  else{
-    Serial.println("ACCESS DENIED");
-    delay(3000);
-  }
+//  if(!mfrc522.PICC_IsNewCardPresent())
+//  {
+//    return;
+//  }
+//  if(!mfrc522.PICC_ReadCardSerial())
+//  {
+//    return;
+//  }
+//  Serial.print("UID Tag : ");
+//  String content= "";
+//  byte letter;
+//  for(byte i = 0;i<mfrc522.uid.size; i++)
+//  {
+//    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+//    Serial.print(mfrc522.uid.uidByte[i], HEX);
+//    content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+//    content.concat(String(mfrc522.uid.uidByte[i], HEX));
+//  }
+//  Serial.println();
+//  content.toUpperCase();
+//  if(content.substring(1) == "09 C2 E6 A2")
+//  {
+//    Serial.println("WELCOME ALI");
+//    Serial.println();
+//    delay(3000);
+//  }
+//  else if(content.substring(1) == "59 91 1C 98")
+//  {
+//    Serial.println("WELCOME REZA");
+//    Serial.println();
+//    delay(3000);
+//  }
+//  else{
+//    Serial.println("ACCESS DENIED");
+//    delay(3000);
+//  }
   delay(2000);
+
 }
 
 void clientRequest()
@@ -103,7 +104,6 @@ void clientRequest()
   {
     delay(1);
   }
-
   request = client.readStringUntil('\r');
   Serial.println(request);
   client.flush();
